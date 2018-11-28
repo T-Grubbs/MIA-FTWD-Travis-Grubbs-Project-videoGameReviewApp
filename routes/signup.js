8c6733ed5b2453e471d4405a102194fa99da7c22
Travis Grubbs <flygrubbs@yahoo.com>
2018-11-28 16:25:46 -0500

    login

--- routes/signup.js
+++ routes/signup.js
@@ -5,53 +5,6 @@ const bcrypt     = require('bcryptjs');
 const bcryptSalt = 10;
 
 
-// const ensureLogin = require("connect-ensure-login")
-// const passport = require("passport");
-
-
-//creates Signup page
-// router.get('/signup', (req, res, next) => {
-//   res.render('signup');
-// });
-
-// router.post('/signup', (req, res, next)=>{
-
-
-//   const theUserName = req.body.theUserName;
-//   const thePassWord = req.body.thePassWord;
-
-//   if (theUserName === "" || thePassWord === "") {
-//       res.render("signup", {errorMessage: "Indicate a username and a password to sign up"});
-//       return;
-//     }
-
-//     User.findOne({username: theUserName })
-//     .then(user => {
-//             if (user !== null) {
-//                 res.render("signup", {
-//                     errorMessage: "Sorry, that username is awesome so obviously it's taken!"
-//                 });
-//                 return;
-//                 }
-    
-
-
-//         const salt     = bcrypt.genSaltSync(bcryptSalt);
-//         const hashPass = bcrypt.hashSync(thePassWord, salt);
-
-
-//         User.create({username: theUserName, password: hashPass})
-//         .then(()=>{
-//             res.redirect('/');
-//         })
-//         .catch((err)=>{
-//             next(err);
-// })
-
-
-// });// end .then for User.findOne
-// });
-
 
 router.get("/signup", (req, res, next) => {
   res.render("signup");
@@ -86,13 +39,44 @@ router.post("/signup", (req, res, next) => {
       lastname: lastname,
     });
 
-    newUser.save((err) => {
-      if (err) {
-        res.render("signup", { message: "Something went wrong" });
-      } else {
-        res.redirect("/test");
-      }
-    });
+    // newUser.save((err) => {
+    //   if (err) {
+    //     res.render("signup", { message: "Something went wrong" });
+    //   } else {
+    //     req.login(newUser, (err)=>{
+    //       if (err){
+    //         next(err);
+    //       } else {
+    //         console.log(req.user + '============d==a=v=vff=')
+    //         res.redirect("/test");
+      
+    //       }
+    //     })
+    //   }
+    // });
+
+newUser.save()
+.then(()=>{
+  console.log(newUser + 'aslkdfakdjfasdfasf=========sdafa==sd')
+  req.login(newUser, (err)=>{
+    if(err){
+      next(err);
+    } else {
+      res.redirect("/test")
+    }
+  })
+})
+.catch((err)=>{
+  next(err)
+})
+
+
+
+
+
+
+
+
   })
   .catch(err => {
     next(err)
