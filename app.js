dc5371c2d180b2ba07d04d20729f7d822dd4873b
Travis Grubbs <flygrubbs@yahoo.com>
2018-11-28 15:10:11 -0500

    added onilne db

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const User         = require('./models/User')
 
 
 mongoose
-  .connect('mongodb://localhost/video-game-review-app', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
